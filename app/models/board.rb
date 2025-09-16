87bd3745261142dce6a506930e0d0c37bef21595
Eduardo Auditore <alfa.fenrir@gmail.com>
2025-09-16 13:54:35 -0600

    generated model relation and validation

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,6 @@
 class Board < ApplicationRecord
   belongs_to :user
+  has_many :tasks, depedent: :destroy
+
+  validates :title, presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :board
   belongs_to :user
+
+  validates :title, presence: true
+  validates :status, inclusion: { in: %w[todo in_progress done] }
 end
